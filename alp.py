5f20e7190c88da6e506a5bc4788de3dfe6362da4
Rohithkumar <40801429+Rohithkumar@users.noreply.github.com>
2018-07-04 09:48:01 +0530

    Create alp.py

---
+++ alp.py
@@ -0,0 +1,10 @@
+alp=input()
+if alp.isalpha():
+    if alp== "a" or alp=="e" or alp=="i" alp=="o" or alp=="u"
+       print("it is an vowel")
+    else:
+       print("it is consonant")    
+ else:
+     print{"invalid entry")
+     
+     
