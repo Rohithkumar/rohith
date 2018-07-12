db5a551ccf0f172bada6ff56fea5a529cafc377a
Rohithkumar <40801429+Rohithkumar@users.noreply.github.com>
2018-07-12 12:03:11 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,5 +1,11 @@
-num=int(raw_input())
+x=0
+y=0
+x,y=input().split()
+x=int(x)
+y=int(y)
+a=[int(i) for i in input().split()]
 sum=0
-for i in range(1,num+1):
-	sum=sum+i
+for i in range(0,y):
+	sum=sum+a[i]
 print(sum)
+
