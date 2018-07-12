44e17eaa313fc2b33345745b81e243205c1b3bc6
Rohithkumar <40801429+Rohithkumar@users.noreply.github.com>
2018-07-12 11:32:38 +0530

    Update alp.py

--- alp.py
+++ alp.py
@@ -2,8 +2,8 @@ ch=input()
 if ch.isalpha():
    if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
  or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-       print("it is an vowel")
+       print("Vowel")
    else:
-    print("it is consonant")    
+    print("Consonant")    
 else:
-    print("invalid entry")
+    print("invalid")
